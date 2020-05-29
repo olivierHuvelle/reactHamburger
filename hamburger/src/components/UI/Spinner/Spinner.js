1d76f2fc32e106eecf6ac983fecd3062857936ec
= <olivier.huvelle@gmail.com>
2020-05-29 09:04:34 +0200

    added Spinner Component to UI Components

---
+++ hamburger/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Spinner.module.css'
+
+/*
+    Required props : NONE
+*/
+
+const Spinner = () => (<div className={classes.Spinner}>Loading...</div>)
+
+export default Spinner

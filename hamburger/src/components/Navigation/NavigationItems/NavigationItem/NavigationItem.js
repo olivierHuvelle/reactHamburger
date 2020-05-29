7074c42971f88c9f47671b72414973d26eea11ad
= <olivier.huvelle@gmail.com>
2020-05-29 13:49:35 +0200

    working on navigationItem(s) components

---
+++ hamburger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NavigationItemt = props => ( 
+    <h2>Hello world</h2>
+)
+
+export default NavigationItem 
---
+++ hamburger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './NavigationItems.module.css'
+
+
+const NavigationItems = props => (
+    <ul>
+        <li><a href="/"></a></li>
+    </ul>
+)
+
+export default NavigationItems 
+
